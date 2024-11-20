df26ee9849c27373075f3462713fee2c3f8cebab
Bart Frackiewicz <decomplosed@gmail.com>
2024-11-20 22:35:49 +0100

    Call API to get stock data

--- stocks/quotes/views.py
+++ stocks/quotes/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+import requests
+import json
+import os
 
 def home(request):
-	return render(request, 'home.html', {})
+	api_request = requests.get(f"https://api.finazon.io/latest/finazon/us_stocks_essential/tickers?ticker=AAPL&apikey={os.getenv('API_KEY')}")
+
+	try:
+		api = json.loads(api_request.content)
+	except Exception as e:
+		api = "Error..."
+
+	return render(request, 'home.html', {'api': api})
 
 def about(request):
 	return render(request, 'about.html', {})
